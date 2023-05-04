539f0951697206b07881a2f6d931dfeb8b654016
Tommy <tommy@MacBook-Tommy.local>
2023-05-04 10:28:36 +0200

    added a function to delete images

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const path = require("path");
 const app = express();
 const port = 5000;
+const fs = require("fs");
 
 app.use(cors());
 app.use(express.json());
@@ -14,15 +15,14 @@ let imageName = "";
 const storage = multer.diskStorage({
   destination: path.join("./image"), 
   filename: function (req, file, cb) {
-	//imageName = Date.now() + path.extname(file.originalname);
-	imageName=file.originalname
+	imageName = Date.now() + path.extname(file.originalname);
 	cb(null, imageName);
   },
 });
 
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 3000000 },
+// limits: { fileSize: 3000000 },
 }).single("myImage");
 
 
@@ -31,11 +31,28 @@ app.post("/upload-image", (req, res) => {
 	if (err) {
 	  console.log(err);
 	} else {
-	  return res.status(201).json({ url: "http://localhost:5000/image/" + imageName });
+	  return res.status(201).json({ url: "http://localhost:5000/image/" + imageName , imageName:imageName});
 	}
   });
 });
 
+app.delete("/delete-image/:imagename", (req, res) => {
+  if (!req.params.imagename){
+	  return res.status(500).json('error in delete');
+  }else{
+	  fs.unlink("image/" + req.params.imagename, (err) => {
+		  if (err) {
+			res.status(500).send({
+			  message: "Could not delete the file. " + err,
+			});
+		  }
+	  
+		  res.status(200).send({
+			message: "File is deleted.",
+		  });
+		});
+	  };
+});
 
 app.listen(port, () => {
   console.log("server run in port", port);
